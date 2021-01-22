cc7f636f45dc2ca3e07a3e35a77a8af747a93224
Xutrurn <kanjoskillz@gmail.com>
2021-01-22 16:28:01 +0100

    aggiunta funzione js per filtrare icone

--- js/script.js
+++ js/script.js
@@ -2,110 +2,141 @@
 
 $(document).ready(function() {
 
+  const icons = [{
+      name: 'cat',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'crow',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dog',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dove',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dragon',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'horse',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'hippo',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'fish',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'carrot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'apple-alt',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'lemon',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'pepper-hot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'user-astronaut',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-graduate',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-ninja',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-secret',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    }
+  ];
+
+  const colors = [
+    'blue',
+    'orange',
+    'purple'
+  ];
+
+  const iconTypes = getTypes(icons);
+  console.log(iconTypes);
 
 
 
-const icons = [{
-    name: 'cat',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-  },
-  {
-    name: 'crow',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-  },
-  {
-    name: 'dog',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-  },
-  {
-    name: 'dove',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-  },
-  {
-    name: 'dragon',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-  },
-  {
-    name: 'horse',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-  },
-  {
-    name: 'hippo',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-  },
-  {
-    name: 'fish',
-    prefix: 'fa-',
-    type: 'animal',
-    family: 'fas',
-  },
-  {
-    name: 'carrot',
-    prefix: 'fa-',
-    type: 'vegetable',
-    family: 'fas',
-  },
-  {
-    name: 'apple-alt',
-    prefix: 'fa-',
-    type: 'vegetable',
-    family: 'fas',
-  },
-  {
-    name: 'lemon',
-    prefix: 'fa-',
-    type: 'vegetable',
-    family: 'fas',
-  },
-  {
-    name: 'pepper-hot',
-    prefix: 'fa-',
-    type: 'vegetable',
-    family: 'fas',
-  },
-  {
-    name: 'user-astronaut',
-    prefix: 'fa-',
-    type: 'user',
-    family: 'fas',
-  },
-  {
-    name: 'user-graduate',
-    prefix: 'fa-',
-    type: 'user',
-    family: 'fas',
-  },
-  {
-    name: 'user-ninja',
-    prefix: 'fa-',
-    type: 'user',
-    family: 'fas',
-  },
-  {
-    name: 'user-secret',
-    prefix: 'fa-',
-    type: 'user',
-    family: 'fas',
-  }
-];
+
 
 
 
 });
+// fine doc
+
+
+
+// Funzione per filtrare icone in base al tipo
+function getTypes(array) {
+  const types = [];
+
+  array.forEach((element) => {
+    if (!types.includes(element.type)) {
+      types.push(element.type);
+    }
+  });
+
+  return types;
+
+}
+
+
+
+
 
 
 
